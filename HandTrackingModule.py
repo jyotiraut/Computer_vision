ac2de4d9cc11a463b4ea89dcf142bc598a594db2
jyotiraut <jyoti.211314@ncit.edu.np>
2025-07-01 23:30:52 +0545

    module done

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -10,13 +10,10 @@ class handDetector():
         self.trackCon = trackCon
         self.mpHands = mp.solutions.hands
         self.hands = self.mpHands.Hands(
-              static_image_mode=self.mode,
-              max_num_hands=self.maxHands,
-              min_detection_confidence=self.detectionCon,
-              min_tracking_confidence=self.trackCon)
-                 
-
-
+            static_image_mode=self.mode,
+            max_num_hands=self.maxHands,
+            min_detection_confidence=self.detectionCon,
+            min_tracking_confidence=self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
 
     def findHands(self, img, draw=True):
@@ -27,8 +24,19 @@ class handDetector():
             for handLms in self.results.multi_hand_landmarks:
                 if draw:
                     self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
-
         return img
+    
+    def findPosition(self, img, handNo=0, draw=True):
+        lmlist = []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                lmlist.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 7, (255, 0, 255), cv2.FILLED)
+        return lmlist
 
 def main():
     cap = cv2.VideoCapture(0)
@@ -38,6 +46,9 @@ def main():
     while True:
         success, img = cap.read()
         img = detector.findHands(img)
+        lmlist = detector.findPosition(img)
+        if len(lmlist) != 0:
+            print(lmlist[3])  # Example: tip of index finger
 
         cTime = time.time()
         fps = 1 / (cTime - pTime)
